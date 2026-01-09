9343451112c0e8c802ebd383b7e8204e3406c77b
sanketjopale <sanketpdjopale@gmail.com>
2026-01-09 13:37:23 +0530

    Program to check whether a given number is a natural number in C

---
+++ 4. Conditional Statements/2. Natural number if-else/natural_if_else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter the number: \n");
+    scanf("%d",&n);
+    if(n>=1){
+        printf("Natural Number\n");
+    }
+    else{
+        printf("Not a Natural Number\n");
+    }
+    return 0;
+}
