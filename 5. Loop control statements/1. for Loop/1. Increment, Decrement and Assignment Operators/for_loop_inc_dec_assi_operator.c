8b55aebd782482bab440674db23af04b6606d62e
sanketjopale <sanketpdjopale@gmail.com>
2026-01-09 15:38:38 +0530

    Program using a for loop to show pre-increment, post-increment, post-decrement and subtraction assignment operators.

---
+++ 5. Loop control statements/1. for Loop/1. Increment, Decrement and Assignment Operators/for_loop_inc_dec_assi_operator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+    for(int i=1;i<=5;++i){
+        printf("%d\n",i);
+    }
+    printf("----\n");
+    for(int i=1;i<=5;i++){
+        printf("%d\n",i);
+    }
+    printf("----\n");
+    for(int i=1;i>=-5;--i){
+        printf("%d\n",i);
+    }
+    printf("----\n");
+    for(int i=1;i>=-5;i--){
+        printf("%d\n",i);
+    }
+    printf("----\n");
+    for(int i = 0; i <= 5; i += 2) {
+        printf("%d\n", i);
+    }
+    printf("----\n");
+    for(int i = 0; i >= -6; i -= 2) {
+        printf("%d\n", i);
+    }
+    printf("----\n");
+  return 0;
+  }
