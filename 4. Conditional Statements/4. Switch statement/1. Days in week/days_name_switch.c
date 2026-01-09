667cd2cabd14ab64adee5024a57d9a45de5cd413
sanketjopale <sanketpdjopale@gmail.com>
2026-01-09 13:39:18 +0530

    Program for days in week
    Program to show name of day, depending on number 1-7.

---
+++ 4. Conditional Statements/4. Switch statement/1. Days in week/days_name_switch.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main() {
+    int day; // 1-mon; 2-tues; 3-wed ...
+    printf("enter day(1-7) : ");
+    scanf("%d", &day);
+    switch (day) {
+        case 1:
+            printf("monday \n");
+            break;
+        case 2:
+            printf("tuesday \n");
+            break;
+        case 3:
+            printf("wednesday \n");
+            break;
+        case 4:
+            printf("thursday \n");
+            break;
+        case 5:
+            printf("friday \n");
+            break;
+        case 6:
+            printf("saturday \n");
+            break;
+        case 7:
+            printf("sunday \n");
+            break;
+        default:
+            printf("Invalid input! Please enter a number between 1 and 7. \n");
+            break;
+    }
+
+    return 0;
+}
