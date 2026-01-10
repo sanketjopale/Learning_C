fe9a2b00866a5f46679bb397ba199a6220f92ea1
sanketjopale <sanketpdjopale@gmail.com>
2026-01-10 11:49:51 +0530

    Program to demonstrate user-defined functions without return value (Hello & Goodbye messages)
    Implement greeting functions to print welcome messages.

---
+++ 6. Functions/1. Greeting using function without return value/greeting_function.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void prtHello();
+void prtGBye();
+int main(){
+    printf("Welcome\n");
+    prtHello();
+    prtGBye();
+    return 0;
+}
+void prtHello(){
+    printf("Hello Sanket\n");
+    printf("Continue learning \n");
+}
+void prtGBye(){
+    printf("Good Bye Sanket\n");
+    printf("See you soon \n");
+}
