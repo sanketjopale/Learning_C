9615ac91c1a7bdcc2984f7cac5e98f540e110c21
sanketjopale <sanketpdjopale@gmail.com>
2026-01-10 11:55:11 +0530

    Program to add two numbers using function with user input and return value

--- 6. Functions/3. Addition in Functions/2. Addition of value given by user/input_add_functions.c
+++ 6. Functions/3. Addition in Functions/2. Addition of value given by user/input_add_functions.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-int add(int x, int y) {
-    return x + y;
+int add(int a, int b){
+    return a+b;
 }
-int main() {
-    int a, b, result;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-    result = add(a, b);
-    printf("Sum = %d", result);
+int main(){
+    int x;
+    int y;
+    int result;
+    printf("Enter first number: \n");
+    scanf("%d",&x);
+    printf("Enter second number: \n");
+    scanf("%d",&y);
+    result=add(x,y);
+    printf("Sum is: %d",result);
     return 0;
 }
